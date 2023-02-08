7c3c0a812a0c088474a286e07bcf9889422cdb53
Siddarth Srikanth <srikanthsiddarth1@gmail.com>
2023-02-07 17:51:24 -0800

    docstrings/comments added

--- TMDB_scraper/TMDB_scraper/spiders/tmdb_spider.py
+++ TMDB_scraper/TMDB_scraper/spiders/tmdb_spider.py
@@ -17,25 +17,43 @@ class TmdbSpider(scrapy.Spider):
 
     def parse(self, response):
         '''
-        DOCSTRING
+        Navigates to the Cast & Crew page from specified movie/TV show page
+        Parameters:
+        - self
+        - response: response to parse request
+        Yields Request with callback to parse_full_credits()
         '''
+        # extracts all links on page that matches specfied format
         links = self.cast_link_extractor.extract_links(response)
-        target = links[1] # hard coded
+        target = links[1] # hard-coded link to Cast & Crew page
         yield Request(target.url, callback = self.parse_full_credits)
 
     def parse_full_credits(self, response):
         '''
-        DOCSTRING
+        Navigates to each actor's page from the specified Cast & Crew page
+        Parameters:
+        - self
+        - response: response to parse request
+        Yields Request with callback to parse_actor_page()
         '''
+        # extracts all actor links on page
         links = self.actor_link_extractor.extract_links(response)
         for link in links:
+            # navigate to each actor's page
             yield Request(link.url, callback = self.parse_actor_page)
 
     def parse_actor_page(self, response):
         '''
-        DOCSTRING
+        Collects all movies/TV shows listed on the specified actor's page
+        Parameters:
+        - self
+        - response: response to parse request
+        Yields a dictionary of the form {"actor" : actor_name, "movie_or_TV_name" : movie_or_TV_name}.
+        for each of the movies or TV shows the actor has worked on
         '''
+        # extract name of actor
         name = response.css("div.title a::text").get()
+        # extract list of all movies/TV shows actor has worked on
         works = response.css("table.credit_group a.tooltip")
         for work in works:
             yield {"actor": name,"movie_or_TV_name": work.css("a bdi::text").get()}
